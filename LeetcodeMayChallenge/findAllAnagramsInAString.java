8154b092ec2228c7366d6244166bc0be0d21b45e
Himanshu Gupta <guptahimanshu479@gmail.com>
2020-05-19 17:50:50 +0530

    May challenge day 17

---
+++ LeetcodeMayChallenge/findAllAnagramsInAString.java
@@ -0,0 +1,34 @@
+package LeetcodeMayChallenge;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class findAllAnagramsInAString {
+	public ArrayList<Integer> findAnagrams(String s, String p) {
+		int slen = s.length();
+		int plen = p.length();
+		ArrayList<Integer> res = new ArrayList<>();
+		int start = 0;
+		int end = 0;
+		HashMap<Character, Integer> map = new HashMap<>();
+		for (int i = 0; i < plen; i++) {
+			map.put(p.charAt(i), map.getOrDefault(p.charAt(i), 0) + 1);
+		}
+		while (end < s.length()) {
+			if (map.containsKey(s.charAt(end)) && map.get(s.charAt(end)) > 0) {
+				map.put(s.charAt(end), map.get(s.charAt(end)) - 1);
+				end++;
+				if (end - start == plen) {
+					res.add(start);
+				}
+			} else if (end == start) {
+				end++;
+				start++;
+			} else {
+				map.put(s.charAt(start), map.get(s.charAt(start)) + 1);
+				start++;
+			}
+		}
+		return res;
+	}
+}
