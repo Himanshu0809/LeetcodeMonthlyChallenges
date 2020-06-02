85dae949d95f75383f7524fa8a65bd1a0fa8b0d8
Himanshu0809 <guptahimanshu479@gmail.com>
2020-06-03 02:09:25 +0530

    June challnege day 2

---
+++ LeetcodeMayChallenge/deleteNodeInLinkedList.java
@@ -0,0 +1,32 @@
+package LeetcodeMayChallenge;
+
+public class deleteNodeInLinkedList {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		linkedList list1 =new linkedList();
+//		list1.addLast(1);
+//		list1.addLast(2);
+//		list1.addLast(3);
+//		list1.addLast(4);
+//		list1.addLast(5);
+//		Node node=list1.new Node(5);
+//		//function call statement
+//	}
+
+	/**
+	 * Definition for singly-linked list.
+	 * public class ListNode {
+	 *     int val;
+	 *     ListNode next;
+	 *     ListNode(int x) { val = x; }
+	 * }
+	 */
+//	class Solution {
+//	    public void deleteNode(ListNode node) {
+//	        node.val=node.next.val;
+//	        node.next=node.next.next;
+//	    }
+//	}
+	
+}
