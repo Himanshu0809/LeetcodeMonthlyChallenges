7c46639c8a1e4e2a7bdcbd7ad5bde86d2bef7675
Himanshu0809 <guptahimanshu479@gmail.com>
2020-06-24 05:23:16 +0530

    June challenge

---
+++ LeetcodeJuneChallenge/countCompleteTreeNodes.java
@@ -0,0 +1,28 @@
+package LeetcodeJuneChallenge;
+
+public class countCompleteTreeNodes {
+	/**
+	 * Definition for a binary tree node.
+	 * public class TreeNode {
+	 *     int val;
+	 *     TreeNode left;
+	 *     TreeNode right;
+	 *     TreeNode() {}
+	 *     TreeNode(int val) { this.val = val; }
+	 *     TreeNode(int val, TreeNode left, TreeNode right) {
+	 *         this.val = val;
+	 *         this.left = left;
+	 *         this.right = right;
+	 *     }
+	 * }
+	 */
+//	class Solution {
+//	    int count=0;
+//	    public int countNodes(TreeNode root) {
+//	        if(root==null){
+//	            return 0;
+//	        }
+//	        return 1+countNodes(root.left)+countNodes(root.right);
+//	    }    
+//	}
+}
